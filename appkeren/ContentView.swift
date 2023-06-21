c0d8d67b9c47d1c0c964c7fce36af2648dd9a60a
nadineaurelia <nadineaurelia11@gmail.com>
2023-06-21 14:42:12 +0700

    nambah gambar meow

--- appkeren/ContentView.swift
+++ appkeren/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("meow")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("nadine aureliaaahhhh!")
