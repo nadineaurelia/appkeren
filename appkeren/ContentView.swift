24180760687a940fbab5c4ec75ff2f7add09a2af
nadineaurelia <nadineaurelia11@gmail.com>
2023-06-21 14:28:40 +0700

    nambain nadine aureliahhhh

--- appkeren/ContentView.swift
+++ appkeren/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("nadine aureliaaahhh!")
+            Text("nadine aureliaaahhhh!")
         }
         .padding()
     }
