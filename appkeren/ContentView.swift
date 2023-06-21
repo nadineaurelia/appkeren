7be502ad526ed8bfdbe80759901fb2ef30565ca5
nadineaurelia <nadineaurelia11@gmail.com>
2023-06-21 14:27:17 +0700

    Initial Commit

---
+++ appkeren/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  appkeren
+//
+//  Created by Nadine Aurelia on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("nadine aureliaaahhh!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appkeren/appkerenApp.swift
@@ -0,0 +1,17 @@
+//
+//  appkerenApp.swift
+//  appkeren
+//
+//  Created by Nadine Aurelia on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct appkerenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
